194f4fbef15a05cd0686cb60004cff3770cb5094
luiz-ortega <luizngust@hotmail.com>
2020-10-22 22:57:13 -0300

    feat: setup redux store and typescript.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Catalog from "./components/Catalog.tsx";
 
 function App() {
-  return <h1>Redux</h1>;
+  return (
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
+  );
 }
 
 export default App;
