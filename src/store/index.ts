c6d2c5077018e2a14ea38250876448989240381d
luiz-ortega <luizngust@hotmail.com>
2020-10-31 19:44:16 -0300

    feat: add cart module saga.

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import { ICartState } from "./modules/cart/types";
 import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./modules/rootReducer";
+import rootSaga from "./modules/rootSaga";
 
 export interface IState {
   cart: ICartState;
@@ -13,5 +14,7 @@ const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
