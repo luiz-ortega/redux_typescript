b96ad9892fb3e8c904a0d67e6b8ba00be45ed4aa
luiz-ortega <luizngust@hotmail.com>
2020-11-01 14:37:57 -0300

    feat: check product quantity on cart reducer current state.

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,7 +1,22 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, select, takeLatest } from "redux-saga/effects";
+import { IState } from "../..";
+import { addProductToCart } from "./actions";
 
-function checkProductStock() {
-  console.log("Adicionou!");
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantity);
+
+  console.log(payload);
 }
 
 export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
